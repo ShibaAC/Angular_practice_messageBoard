23c6088da8bd345082cef9b549141e2f0c010859
Shiba AC <shiba19981216@gmail.com>
2023-09-05 15:34:31 +0800

    Angular留言板練習(事件綁定、差值綁定、雙向綁定)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from './message';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyMessageBoard';
+  name = '';
+  content = '';
+  messages: Message[] = [];
+
+
+  // trim()用來去除字串兩邊的空格
+  addMessage():void {
+    //如果有其中一個或兩個都空白 不執行後面
+    if(!this.name.trim() || !this.content.trim()){
+      return;
+    }
+    //創建message然後把this.name跟this.content傳入建構函數
+    const message = new Message(this.name , this.content);
+    //.push()=>加到陣列最尾端
+    this.messages.push(message);
+    //把輸入框清空
+    this.content = '';
+  }
 }
